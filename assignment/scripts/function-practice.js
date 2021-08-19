d0a939f7b210191dd33388e58985cb491268e3b6
Amanda Zimmerman <amanda.zimms@gmail.com>
2021-08-18 21:12:24 -0500

    finished assignment, started on stretch goals

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -12,64 +12,115 @@ function hello() {
 console.log('Test - should say "Hello World!"', hello());
 
 
-// 2. Function to return an personalized hello, using the `name` argument.
+// 2. Function to return a personalized hello, using the `name` argument.
 //    for example 'Hello, Jo!', or 'Hello, Stacy!'
 function helloName( name ) {
-  return;
+  return 'Hello, ' + name;
 }
 // Remember to call the function to test
-
+console.log('helloName(Juanjo) returns:', helloName('Juanjo'));
 
 // 3. Function to add two numbers together & return the result
-function addNumbers( firstNumber ) {
+function addNumbers( firstNumber, secondNumber) {
   // return firstNumber + secondNumber;
+  return firstNumber + secondNumber;
 }
+console.log('addNumbers(99,1) returns:', addNumbers(99,1));
 
 
 // 4. Function to multiply three numbers & return the result
-function multiplyThree( ){
-
+function multiplyThree( num1, num2, num3 ){
+  return num1 * num2 * num3;
 }
+console.log('multiplyThree(2,3,4) returns:', multiplyThree(2,3,4));
+
 
 
 // 5. Function that will return true if a number is positive, 
 //    or greater than zero, and false otherwise
 function isPositive( number ) {
   if ( number > 0 ){
-    return;
+    return true;
+  } else if ( number < 0 ){
+    return false;
+  } else if ( number === 0 ) {
+    return "zero is neither positive nor negative"
   }
-    return;
 }
 // Call the function to test each outcome (true & false) 
 // Write a separate console.log statement for each outcome
-console.log( 'isPositive - should say true', isPositive(3) );
-console.log( 'isPositive - should say false', isPositive(0) );
-console.log( 'isPositive - should say false', isPositive(-3) );
+console.log('isPositive(-9) returns:', isPositive(-9));
+console.log('isPositive(8) returns:', isPositive(8));
+console.log('isPositive(-222) returns:', isPositive(-222));
+console.log('isPositive(18) returns:', isPositive(18));
+console.log('isPositive(0) returns:', isPositive(0));
 
 
 // 6. Function to return the _last_ item in an array. If the 
 //    array is empty, return `undefined`.
 function getLast( array ) {
-
+  return array[array.length -1];
 }
+let testArray = [0,1,2,3];
+console.log('getLast(testArray), where testArray = [0,1,2,3], returns:', getLast(testArray));
+
 
 // 7. Function to find a value in an array. Return true if the 
 //    value is found and false otherwise. Use a loop;
 //    DO NOT use Array.includes, Array.indexOf, or Array.find 
 function find( value, array ){
-  
+
+  let foundMatch = false; //declare and set a bool to false
+  for (const item of array) { //loop through items in array
+      if (value === item) { //if any of the items are equal to value
+        foundMatch = true; //set that bool to true
+      } //(else it will remain false anyway)
+  }
+
+  if (foundMatch === true) { //check if the bool was ever flipped to true
+      return true;
+  } else {
+      return false;
+  }
 }
+console.log('find(2, testArray), where testArray = [0,1,2,3], returns:', find(2, testArray));
+console.log('find(7, testArray), where testArray = [0,1,2,3], returns:', find(7, testArray));
+
 
 // ----------------------
 // Stretch Goals
 // ----------------------
-// 8. Function to check if a letter is the first letter in a 
+// 8a. Function to check if a letter is the first letter in a 
 //    string. Return true if it is, and false otherwise
 function isFirstLetter(letter, string) {
+
+  if (string.charAt(0) === letter){
+    return true;
+  } else {
+    return false;
+  }
 
 }
 console.log( 'isFirstLetter - should say true', isFirstLetter('a', 'apple') );
 console.log( 'isFirstLetter - should say false', isFirstLetter('z', 'apple') );
+
+
+// 8b. I was not sure if the above solution 8a counts as cheating - in the
+//    similar example with arrays above you ask us not to use indexOf which 
+//    feels similar to charAt. So here is another way to solve without charAt:
+function isFirstLetterAnalog(letter, string) {
+
+  
+  if (string.charAt(0) === letter){
+    return true;
+  } else {
+    return false;
+  }
+
+}
+console.log( 'isFirstLetter - should say true', isFirstLetter('a', 'apple') );
+console.log( 'isFirstLetter - should say false', isFirstLetter('z', 'apple') );
+
 
 // 9. Function to return the sum of all numbers in an array
 function sumAll( ) {
